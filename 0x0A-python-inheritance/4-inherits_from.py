bef6932068cbbf4ccbcc1379fb903ad203df61f7
ZakariaAdan <ZakariaAdan0337@gmail.com>
2024-04-25 09:52:15 -0700

    7-base_geometry.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,7 +9,5 @@ def inherits_from(obj, a_class):
        that inherited (directly or indirectly) from the specified class;
        otherwise False.
     """
-    if type(obj) != a_class:
-        return issubclass(type(obj), a_class)
-    else:
-        return False
+    return isinstance(obj, a_class) and type(obj) != a_class
+
